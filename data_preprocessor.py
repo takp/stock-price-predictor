54be47b31032d4d43fdb0794ab895f1d4d7d7835
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2017-11-12 16:32:01 +0900

    Merge the original data to 1 data

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from datetime import timedelta
 
 def preprocess_nikkei(data):
     dropping_features = ['Open Price', 'High Price', 'Low Price']
@@ -21,17 +21,54 @@ def preprocess_currency(data):
 
 def preprocess_data(data, dropping_features):
     data = data.drop(dropping_features, axis=1)
-    data.columns = ['Date', 'ClosePrice']
-    data['Date'] = pd.to_datetime(data['Date'], format='%Y-%m-%d')
+    data.columns = ['date', 'ClosePrice']
+    data['date'] = pd.to_datetime(data['date'], format='%Y-%m-%d')
     data = calculate_change_rate(data)
     return data
 
 
 def calculate_change_rate(data):
-    data['ChangeRate'] = pd.Series()
+    data['change_rate'] = pd.Series()
     for i in range(len(data) - 1):
         close_prices = data['ClosePrice']
         change_rate = (float(close_prices[i]) / close_prices[i + 1]) * 100
         rounded_change_rate = round(change_rate, 50)
-        data.set_value(i, 'ChangeRate', rounded_change_rate)
+        data.set_value(i, 'change_rate', rounded_change_rate)
     return data
+
+
+def merge(nikkei_data, nasdaq_data, currency_data):
+    df = create_blank_dataframe()
+    df['nikkei'] = pd.Series()
+    df['nasdaq'] = pd.Series()
+    df['currency'] = pd.Series()
+    # Input each data
+    for i in range(len(df)):
+        date = df.loc[i, 'date']
+        nikkei = nikkei_data.loc[(nikkei_data['date'] == date)]
+        if not nikkei.empty:
+            df.loc[i, 'nikkei'] = nikkei['change_rate'].item()
+        nasdaq = nasdaq_data.loc[(nasdaq_data['date'] == date)]
+        if not nasdaq.empty:
+            df.loc[i, 'nasdaq'] = nasdaq['change_rate'].item()
+        currency = currency_data.loc[(currency_data['date'] == date)]
+        if not currency.empty:
+            df.loc[i, 'currency'] = currency['change_rate'].item()
+    return df
+
+
+def create_blank_dataframe():
+    today = pd.to_datetime('today')
+    start = pd.to_datetime('2013-01-22')
+    dates = []
+    for i in daterange(start, today):
+        # print(i.strftime("%Y-%m-%d"))
+        dates.append(pd.to_datetime(i))
+    df = pd.DataFrame(dates, columns=['date'])
+    return df
+
+
+def daterange(start, end):
+    for i in range(int ((end - start).days)):
+        yield start + timedelta(i)
+
--- main.py
+++ main.py
@@ -11,29 +11,31 @@ nikkei_data = data_preprocessor.preprocess_nikkei(nikkei_data_org)
 nasdaq_data = data_preprocessor.preprocess_nasdaq(nasdaq_data_org)
 currency_data = data_preprocessor.preprocess_currency(currency_data_org)
 
-print(nikkei_data.head())
-print(nasdaq_data.head())
-print(currency_data.head())
+# Merge data
+merged_data = data_preprocessor.merge(nikkei_data, nasdaq_data, currency_data)
+print(merged_data.head(20))
 
-# Split the data
-nikkei_train_data, nikkei_test_data = data_splitter.split(nikkei_data)
-print("Nikkei train dataset has {} samples.".format(*nikkei_train_data.shape))
-print("Nikkei test dataset has {} samples.".format(*nikkei_test_data.shape))
-# print(nikkei_train_data.head())
-# print(nikkei_test_data.head())
+#
+#
+# # Split the data
+# nikkei_train_data, nikkei_test_data = data_splitter.split(nikkei_data)
+# print("Nikkei train dataset has {} samples.".format(*nikkei_train_data.shape))
+# print("Nikkei test dataset has {} samples.".format(*nikkei_test_data.shape))
+# # print(nikkei_train_data.head())
+# # print(nikkei_test_data.head())
+#
+# nasdaq_train_data, nasdaq_test_data = data_splitter.split(nasdaq_data)
+# print("Nasdaq train dataset has {} samples.".format(*nasdaq_train_data.shape))
+# print("Nasdaq test dataset has {} samples.".format(*nasdaq_test_data.shape))
+# # print(nasdaq_train_data.head())
+# # print(nasdaq_test_data.head())
+#
+# currency_train_data, currency_test_data = data_splitter.split(currency_data)
+# print("Currency train dataset has {} samples.".format(*currency_train_data.shape))
+# print("Currency test dataset has {} samples.".format(*currency_test_data.shape))
+# # print(currency_train_data.head())
+# # print(currency_test_data.head())
 
-nasdaq_train_data, nasdaq_test_data = data_splitter.split(nasdaq_data)
-print("Nasdaq train dataset has {} samples.".format(*nasdaq_train_data.shape))
-print("Nasdaq test dataset has {} samples.".format(*nasdaq_test_data.shape))
-# print(nasdaq_train_data.head())
-# print(nasdaq_test_data.head())
-
-currency_train_data, currency_test_data = data_splitter.split(currency_data)
-print("Currency train dataset has {} samples.".format(*currency_train_data.shape))
-print("Currency test dataset has {} samples.".format(*currency_test_data.shape))
-# print(currency_train_data.head())
-# print(currency_test_data.head())
-
-# Build model
+# Build & train model
 model = lstm_model.LSTMModel().build()
-
+# model.fit(X_train, y_train, batch_size=10, nb_epoch=0.05)
