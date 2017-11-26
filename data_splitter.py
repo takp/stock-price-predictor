de588845d5e9a64e21b15ae20934ac59b1180d50
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2017-11-26 18:01:06 +0700

    Use validation data

--- data_splitter.py
+++ data_splitter.py
@@ -12,11 +12,14 @@ class DataSplitter:
         return numpy.array(x), numpy.array(y)
 
     @classmethod
-    def split_to_train_and_test(self, data):
+    def split_to_train_val_test(self, data):
         train_start = pd.to_datetime('2013-01-22')
+        validation_start = pd.to_datetime('2016-07-01')
         test_start = pd.to_datetime('2017-01-01')
-        train_data_mask = (train_start <= data['date']) & (data['date'] < test_start)
+        train_data_mask = (train_start <= data['date']) & (data['date'] < validation_start)
+        validation_data_mask = (validation_start <= data['date']) & (data['date'] < test_start)
         test_data_mask = (test_start <= data['date']) & (data['date'] < pd.to_datetime('today'))
         train_data = data.loc[train_data_mask]
+        validation_data = data.loc[validation_data_mask]
         test_data = data.loc[test_data_mask]
-        return train_data, test_data
+        return train_data, validation_data, test_data
--- main.py
+++ main.py
@@ -26,13 +26,17 @@ merged_data = DataPreprocessor.merge(nikkei_data, nasdaq_data, currency_data)
 data = merged_data.dropna()
 
 # Split the data
-data_train, data_test = DataSplitter.split_to_train_and_test(data)
+data_train, data_val, data_test = DataSplitter.split_to_train_val_test(data)
 x_train, y_train = DataSplitter.split_to_x_and_y(data_train, timesteps=timesteps)
+x_val, y_val = DataSplitter.split_to_x_and_y(data_val, timesteps=timesteps)
 x_test, y_test = DataSplitter.split_to_x_and_y(data_test, timesteps=timesteps)
 
 print("Train dataset has {} samples.".format(*x_train.shape))
 # print(x_train[:3])
 # print(y_train[:3])
+print("Validation dataset has {} samples.".format(*x_val.shape))
+# print(x_val[:3])
+# print(y_val[:3])
 print("Test dataset has {} samples.".format(*x_test.shape))
 # print(x_test[:3])
 # print(y_test[:3])
@@ -40,7 +44,8 @@ print("Test dataset has {} samples.".format(*x_test.shape))
 # Build & train model
 model = LSTMModel(timesteps, hidden_neurons).build()
 print("Fitting the model...")
-model.fit(x_train, y_train, batch_size=batchsize, epochs=epochs)
+model.fit(x_train, y_train,
+          batch_size=batchsize, epochs=epochs, validation_data=(x_val, y_val))
 
 print("Predicting...")
 result = model.predict(x_test)
@@ -54,3 +59,5 @@ print(predicted[:50])
 
 # Output to csv
 predicted.to_csv("predicted.csv")
+
+# TODO: Compare with benchmark model
